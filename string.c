5d14caf06effa09db34cee2c92d78e95f3eee99f
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2023-09-14 21:57:17 +0500

    Add files via upload

---
+++ string.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int summCounter[28] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '\0'};
+	for (int number1 = 0; number1 < 10; number1++)
+		for (int number2 = 0; number2 < 10; number2++)
+			for (int number3 = 0; number3 < 10; number3++)
+				summCounter[number1 + number2 + number3]++;
+	int Tickets = 0;
+	int numberArraySymbol = 0;
+	for (numberArraySymbol; numberArraySymbol < 28; numberArraySymbol++)
+	{
+		summCounter[numberArraySymbol] = summCounter[numberArraySymbol] * summCounter[numberArraySymbol];
+		Tickets = Tickets + summCounter[numberArraySymbol];
+	}
+	printf("Number of lucky tickets %d", Tickets);
+}
+
+
+
+
